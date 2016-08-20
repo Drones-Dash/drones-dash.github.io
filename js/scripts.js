2a8a7e73dc96698a463a10737419fff7efebe1f5
Ayush Jain <ayushajain@gmail.com>
2016-08-20 03:05:40 -0700

    content

--- js/scripts.js
+++ js/scripts.js
@@ -56,7 +56,7 @@ $(document).ready(function() {
 			if (scroll >= 20) {
 				$('section.navigation').addClass('fixed');
 				$('header').css({
-					"border-bottom": "none",
+					"border-bottom": "solid 1px rgb(255, 255, 255)",
 					"padding": "35px 0"
 				});
 				$('header .member-actions').css({
